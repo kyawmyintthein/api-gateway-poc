4a8df329547a6f09fd0e53a2f7d544ea025f2353
Kyaw Myint Thein <kyaw.myintthein@razer.com>
2021-09-17 13:50:22 +0800

    Add requestBodyTransformer and QueryParam

--- main.go
+++ main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"os"
 
-	"github.com/luraproject/lura/config"
-	"github.com/luraproject/lura/logging"
+	gologging "github.com/devopsfaith/krakend-gologging"
+	martian "github.com/devopsfaith/krakend-martian"
+	viper "github.com/devopsfaith/krakend-viper"
+	"github.com/gin-gonic/gin"
+	_ "github.com/kpacha/martian-components/body/querystring2body"
+	_ "github.com/kyawmyintthein/api-gateway-poc/requestbodytransformer"
 	"github.com/luraproject/lura/proxy"
-	"github.com/luraproject/lura/router/gin"
+	krakendgin "github.com/luraproject/lura/router/gin"
+	"github.com/luraproject/lura/transport/http/client"
+	"github.com/luraproject/lura/transport/http/server"
 )
 
 func main() {
 	port := flag.Int("p", 0, "Port of the service")
-	logLevel := flag.String("l", "DEBUG", "Logging level")
 	debug := flag.Bool("d", false, "Enable the debug")
 	configFile := flag.String("c", "./configuration.json", "Path to the configuration filename")
 	flag.Parse()
 
-	parser := config.NewParser()
+	parser := viper.New()
 	serviceConfig, err := parser.Parse(*configFile)
 	if err != nil {
 		log.Fatal("ERROR:", err.Error())
@@ -28,9 +34,27 @@ func main() {
 		serviceConfig.Port = *port
 	}
 
-	logger, _ := logging.NewLogger(*logLevel, os.Stdout, "[LURA]")
+	logger, err := gologging.NewLogger(serviceConfig.ExtraConfig, os.Stdout)
+	if err != nil {
+		log.Fatal("ERROR:", err.Error())
+	}
 
-	routerFactory := gin.DefaultFactory(proxy.DefaultFactory(logger), logger)
+	logger.Debug("config:", serviceConfig)
 
-	routerFactory.New().Run(serviceConfig)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	backendFactory := martian.NewBackendFactory(logger, client.DefaultHTTPRequestExecutor(client.NewHTTPClient))
+
+	routerFactory := krakendgin.NewFactory(krakendgin.Config{
+		Engine:         gin.Default(),
+		Logger:         logger,
+		Middlewares:    []gin.HandlerFunc{},
+		HandlerFactory: krakendgin.EndpointHandler,
+		ProxyFactory:   proxy.NewDefaultFactory(backendFactory, logger),
+		RunServer:      server.RunServer,
+	})
+
+	routerFactory.NewWithContext(ctx).Run(serviceConfig)
+
+	cancel()
 }
